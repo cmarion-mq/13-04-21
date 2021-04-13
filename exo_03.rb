219c7072de104fe1ece87492c77f44daecc63bf4
cmarion-mq <cmarion.mq@gmail.com>
2021-04-13 12:15:37 +0200

    Second commit

---
+++ exo_03.rb
@@ -0,0 +1,4 @@
+puts "Quelle est ton année de naissance?"
+print ">"
+user_year = gets.chomp
+puts "Tu avais #{2017 - user_year.to_i} ans en 2017"
---
+++ exo_04.rb
@@ -0,0 +1,4 @@
+puts "Quelle est ton année de naissance?"
+print ">"
+user_year = gets.chomp
+puts "En #{user_year.to_i + 100} tu auras 100 ans"
